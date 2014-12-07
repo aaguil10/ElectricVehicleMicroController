ecac24c694b9a2b95ec67b03155ff94dc42722af
benjaminran <benjaminran2@gmail.com>
2014-12-06 20:31:52 -0800

    updated pwm_input.ino with code to read PWM input from digital pins

--- pwm_input.ino
+++ pwm_input.ino
@@ -1,5 +1,20 @@
-double readPWM(int pin) {
-    double h = pulseIn(pin, HIGH);
-    double l = pulseIn(pin, LOW);
-    return h / (h + l);
+const int in = 2;
+double sensorValue;
+double reading;
+int pin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  sensorValue = takeReading();
+  Serial.println(sensorValue);
+}
+
+double takeReading() {
+  double h = pulseIn(pin,HIGH);
+  double l = pulseIn(pin,LOW);
+  return h / (h+l);
 }
